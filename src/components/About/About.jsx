3adeaa22ad2144dacbc2945b39dedb884ecbb217
alexpichel <alex.pichel@oregon.gov>
2018-11-27 09:14:04 -0800

    another about update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-      <h1>About</h1>
+        <h1>About</h1>
         <p>
           This is a beta site of the
           {' '}
