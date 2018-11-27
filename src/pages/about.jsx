f1e3788524dfa67360a5e1d1699a60541b81bbf1
alexpichel <alex.pichel@oregon.gov>
2018-11-27 09:13:43 -0800

    Updated about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,101 @@
-import React, { Component } from "react";
+import React from "react";
 import Helmet from "react-helmet";
+import { Link } from "react-scroll";
 import About from "../components/About/About";
 import config from "../../data/SiteConfig";
+import Drawer from "../components/Drawer/Drawer";
+import Navigation from "../components/Navigation/Navigation";
+import SiteWrapper from "../components/SiteWrapper/SiteWrapper";
+import Footer from "../components/Footer/Footer";
+import MainHeader from "../components/MainHeader/MainHeader";
+import MainNav from "../components/MainNav/MainNav";
+import BlogLogo from "../components/BlogLogo/BlogLogo";
+import MenuButton from "../components/MenuButton/MenuButton";
+import PageTitle from "../components/PageTitle/PageTitle";
+import PageDescription from "../components/PageDescription/PageDescription";
+import Layout from "../components/layout";
 
-class AboutPage extends Component {
+class AboutPage extends React.Component {
+  state = {
+    menuOpen: false
+  };
+
+  handleOnClick = evt => {
+    evt.stopPropagation();
+    if (this.state.menuOpen) {
+      this.closeMenu();
+    } else {
+      this.openMenu();
+    }
+  };
+
+  handleOnClose = evt => {
+    evt.stopPropagation();
+    this.closeMenu();
+  };
+
+  openMenu = () => {
+    this.setState({ menuOpen: true });
+  };
+
+  closeMenu = () => {
+    this.setState({ menuOpen: false });
+  };
+
   render() {
+
     return (
-      <div className="about-container">
-        <Helmet title={`About | ${config.siteTitle}`} />
-        <About />
-      </div>
+      <Layout location={this.props.location}>
+        <Drawer className="home-template" isOpen={this.state.menuOpen}>
+          <Helmet title={config.siteTitle} />
+
+          {/* The blog navigation links */}
+          <Navigation config={config} onClose={this.handleOnClose} />
+
+          <SiteWrapper>
+            {/* All the main content gets inserted here */}
+            <div className="home-template">
+              {/* The big featured header */}
+              <MainHeader cover={config.siteCover}>
+                <MainNav overlay={config.siteCover}>
+                  <BlogLogo title={config.siteTitle} />
+                  <MenuButton
+                    navigation={config.siteNavigation}
+                    onClick={this.handleOnClick}
+                  />
+                </MainNav>
+                <div className="vertical">
+                  <div className="main-header-content inner">
+                    <img src="/logos/logo-192x192.png" alt="Green book in shape of Oregon (OKB Logo)" />
+                    <PageTitle text={config.siteTitle} />
+                    <PageDescription text={config.siteDescription} />
+                  </div>
+                </div>
+                <Link
+                  className="scroll-down icon-arrow-left"
+                  to="content"
+                  data-offset="-45"
+                  spy
+                  smooth
+                  duration={500}
+                >
+                  <span className="hidden">Scroll Down</span>
+                </Link>
+              </MainHeader>
+              <div className="about-container">
+                <Helmet title={`About | ${config.siteTitle}`} />
+                <About />
+              </div>
+            </div>
+
+            {/* The tiny footer at the very bottom */}
+            <Footer
+              copyright={config.copyright}
+              promoteGatsby={config.promoteGatsby}
+            />
+          </SiteWrapper>
+        </Drawer>
+      </Layout>
     );
   }
 }
